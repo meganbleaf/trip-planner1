a613869ca2eab16feeb2b7d4226b477ab3840d44
Ariel Wainer <arielwainer4@gmail.com>
2020-06-29 12:11:38 -0400

    completed through 4.Map Markers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log("I'm in the index.js file!")
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1IjoiYXJpZWx3YWluZXI0IiwiYSI6ImNrYzBvbnFubDB4bGEyc2xtZXlueDl0djQifQ.JjvlALycYs7uHoIlcPTM8w";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
+
+
